be35fea3d2ae0be725f10fc66974775bc12739c2
MujtabaMuhammad <34104990+MujtabaMuhammad@users.noreply.github.com>
2024-08-02 17:24:44 -0400

    Added pdf validation through js

--- script.js
+++ script.js
@@ -5,10 +5,20 @@ form.addEventListener('submit', handleSubmit);
 
 
 
+
 function handleSubmit(event){
+    
 event.preventDefault();
 const fileInput = document.getElementById('myFile');
 const file = fileInput.files;
 
+if (file[0].type !== "application/pdf")
+    {
+    console.log('Not a pdf file');
+    return;
+} else {
+
 console.log("Name of the file is: " + file[0].name );
+
+}
 }
